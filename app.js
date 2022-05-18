728892c1142d8a327d0ef29b5be0db005cef5307
twój login do GitHuba <96842769+MaksBernatowicz@users.noreply.github.com>
2022-05-18 15:56:40 +0200

    added collision detector for rocket and border and added styling

--- app.js
+++ app.js
@@ -14,20 +14,27 @@ for (let i = 0; i < 255; i++) {
 }
 
 const squares = Array.from(document.querySelectorAll('.grid div'));
-const btnLeft = document.querySelector('.btn-left');
-const btnRight = document.querySelector('.btn-right');
+
+const border = [
+    0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,
+    240,241,242,243,244,245,246,247,248,249,250,251,252,253,254
+];
 
 const alienInvaders = [
     15,16,17,18,19,20,21,22,23,24,
     30,31,32,33,34,35,36,37,38,39,
     45,46,47,48,49,50,51,52,53,54
-]
+];
 
 function draw() {
     for (let i = 0; i < alienInvaders.length; i++) {
         if (!aliensRemoved.includes(i)) {
             squares[alienInvaders[i]].classList.add('invader');
         }
+    }
+
+    for (let i = 0; i < border.length; i++) {
+        squares[border[i]].classList.add('border');
     }
 }
 
@@ -92,8 +99,9 @@ function shootBtn() {
             aliensRemoved.push(alienRemoved);
             results++;
             resultDisplay.innerHTML = results;
-        } else {
-            setTimeout(() => (squares[currentLaserIndex].classList.remove('laser') && clearInterval(laserId)), 10);
+        } else if (squares[currentLaserIndex].classList.contains('border')) {
+            squares[currentLaserIndex].classList.remove('laser');
+            clearInterval(laserId);
         }
     }
     shootSound.play();
@@ -132,11 +140,9 @@ function moveInvaders() {
         clearInterval(invadersId);
     }
 
-    for (let i = 0; i < alienInvaders.length; i++) {
-        if(alienInvaders[i] > 255) {
-            resultDisplay.innerHTML = 'GAME OVER';
-            clearInterval(invadersId);
-        }
+    if (alienInvaders.some((alienInvader) => squares[alienInvader].classList.contains('border'))) {
+        resultDisplay.innerHTML = 'GAME OVER';
+        clearInterval(invadersId);
     }
 
     if (aliensRemoved.length === alienInvaders.length) {
@@ -171,13 +177,14 @@ function shoot(e) {
             aliensRemoved.push(alienRemoved);
             results++;
             resultDisplay.innerHTML = results;
-        } else {
-            setTimeout(() => (squares[currentLaserIndex].classList.remove('laser') && clearInterval(laserId)), 10);
+        } else if (squares[currentLaserIndex].classList.contains('border')) {
+            squares[currentLaserIndex].classList.remove('laser');
+            clearInterval(laserId);
         }
     }
 
-    switch (e.key) {
-        case ' ':           // empty string ' ' for spacebar key
+    switch (e.key) {        //  empty string ' '
+        case ' ':           //  for spacebar key
             shootSound.play();
             laserId = setInterval(moveLaser, 50);
             break;
